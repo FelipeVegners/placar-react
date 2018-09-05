fb4027aa692ebbaa9ac18aaba3b9be32e3012459
Felipe Vegners <felipevegners@gmail.com>
2018-09-05 18:20:04 -0300

    Placar Container Styles

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 import BotaoGol from './BotaoGol'
+import './PlacarContainer.css'
 
 export default class Time extends React.Component {
     render() {
         return (
             <div>
                 <h1>{ this.props.nome }</h1>
-                <h2>{ this.props.gols }</h2>
+                <h2 className="view-goals">{ this.props.gols }</h2>
                 <BotaoGol marcarGol={ this.props.marcarGol } />
             </div>
         )
